cd7b241f86abe132eeb9f7f6d7fd3d6c32edd58a
iBogdanov <ilya.bogdanov14@gmail.com>
2017-01-05 23:27:34 +0300

    Скрипт для форматирования цены

--- format_price.py
+++ format_price.py
@@ -1,5 +1,47 @@
 def format_price(price):
-    pass
+    float_price = float(price)
+    sign = '-' if float_price < 0 else ''
+    digits_count = 2
+    float_price = round(abs(float_price), digits_count)
+    whole_part = int(float_price)
+    remove_signs_count = 2
+    fractional_part = str(round(float_price - whole_part, digits_count))[remove_signs_count:]
+    whole_part = str(whole_part)
+
+    positions_count = 3
+    formatted_price = ''
+    while whole_part:
+        whole_part, last3 = whole_part[:-positions_count], whole_part[-positions_count:]
+        formatted_price = '{0} {1}'.format(last3, formatted_price) if formatted_price else last3
+
+    formatted_price = '{0}{1}{2}'.format(
+        sign,
+        formatted_price,
+        ',{0}'.format(fractional_part) if fractional_part != '0' else ''
+    )
+
+    return formatted_price
 
 if __name__ == '__main__':
-    pass
+    import argparse
+    import logging
+
+    def get_input_data():
+        parser = argparse.ArgumentParser(description='Script for price formatting')
+        parser.add_argument('price', help='Price string')
+        return parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'[%(filename)s#] %(levelname)-8s [%(asctime)s] %(message)s',
+        datefmt=u'%m/%d/%Y %I:%M:%S %p'
+    )
+
+    args = get_input_data()
+    price_string = args.price
+
+    try:
+        formatted_price = format_price(price_string)
+        logging.info('Formatted price: %s', formatted_price)
+    except ValueError as error:
+        logging.exception('Incorrect price string')
--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_price(self):
+        test_prices = ['3245.000000', '-3245.000000', '3245.1399', '-3245.1399', '123456.999']
+        formatted_prices = ['3 245', '-3 245', '3 245,14', '-3 245,14', '123 457']
+        for test_pair in zip(test_prices, formatted_prices):
+            with self.subTest(test_pair=test_pair):
+                self.assertEqual(format_price(test_pair[0]), test_pair[1])
+
+    def test_incorrect_price_raises(self):
+        incorrect_prices = ['3 245.000000', '3245,000000', 'abc', '- 3245.1399']
+        for incorrect_price in incorrect_prices:
+            with self.subTest(incorrect_price=incorrect_price), self.assertRaises(ValueError):
+                format_price(incorrect_price)
+
+
+if __name__ == '__main__':
+    unittest.main()
